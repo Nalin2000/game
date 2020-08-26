d7a51cfc8666d713e37f2ef64d4c43b7f7911dc3
Nalin2000 <nalinsaytongchang@gmail.com>
2020-08-26 22:55:22 +0700

    new2

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,102 @@
 <template>
-  <div id="app">
-    <!--<div class="a">
-    
-
-
-    </div>-->
-     <infinity></infinity>
+  <div id="app" class="playGameScreen">
+<div>
+  <div class="a">
+    <div class="row">
+      <div class="col-md-6">
+      
+        <h1>Hero : {{he_name}} | HP : {{he_hp}}</h1>
+      </div>
+      <div class="col-md-6">
+        <h1>Monster : {{mon_name}} | HP : {{mon_hp}}</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <img :src="h_img" alt class="img-fluid image" :width="he_hp + 'px'" />
+      </div>
+      <div class="col-md-6">
+        <img :src="m_img" alt class="img-fluid image" :width=" mon_hp + 'px'" />
+      </div>
+    </div>
+   </div> 
+     <infinity
+      @H_name="h_name"
+      @H_hp="h_hp"
+      @H_image="h_image"
+      @M_name="m_name"
+      @M_hp="m_hp"
+      @M_image="m_image"
+      @P_atk="p_atk"
+      @M_atk="m_atk"
+      @P_SATK="p_satk"
+      @M_SPATK="m_satk"
+    ></infinity>
+   </div> 
   </div>
 </template>
 
 <script>
-import infinity from "@/components/infinity";
-
+import infinity from "./components/infinity";
 export default {
   name: "App",
   components: {
     infinity,
+  },
+  data: function () {
+    return {
+      he_name: "",
+      mon_name: "",
+      he_hp: 0,
+      mon_hp: 0,
+      attack: 0,
+      h_img: "",
+      m_img: "",
+    };
+  },
+  methods: {
+    //HERO
+    h_name(value) {
+      this.he_name = value;
+      console.log("emit", this.he_name);
+    },
+    h_hp(value) {
+      this.he_hp = value;
+      console.log("emit", this.he_hp);
+    },
+    h_image(value) {
+      this.h_img = value;
+      console.log("emit", this.h_img);
+    },
+    //MONSTER
+    m_name(value) {
+      this.mon_name = value;
+    },
+    m_hp(value) {
+      this.mon_hp = value;
+    },
+    m_image(value) {
+      this.m_img = value;
+    },
+    //ATTACK HERO
+    p_atk(value) {
+      this.attack = value;
+      this.mon_hp -= this.attack;
+      console.log("emit", this.mon_hp);
+    },
+    p_satk(value) {
+      this.attack = value;
+      this.mon_hp -= this.attack;
+    },
+    //ATTACK MONSTER
+    m_atk(value) {
+      this.attack = value;
+      this.he_hp -= this.attack;
+    },
+    m_satk(value) {
+      this.attack = value;
+      this.he_hp -= this.attack;
+    },
   },
 };
 </script>
@@ -26,9 +107,18 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ffffff;
 }
-
-
+.playGameScreen {
+  background-image: url("assets/img/bg1.jpg");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  height: 100vh;
+}
+.a {
+    margin: auto;
+  width: 70%;
+  height: 500px;
+  border: 10px solid black;
+}
 </style>
--- src/components/infinity.vue
+++ src/components/infinity.vue
@@ -1,23 +1,11 @@
 <template>
-  <div>
-    <div class="a">
-      <hr />
-      name = {{aPlayer.name}}
-      hp = {{aPlayer.hp}}
-      image = {{aPlayer.image}}
-      attack = {{this.randomNO(3,10)}}
-      <hr />
-      name = {{bMonster.name}}
-      hp = {{bMonster.hp}}
-      image = {{bMonster.image}}
-      attack = {{this.randomNO(3,10)}}
-    </div>
+  <div class>
     <!-- {{player[randomNO(1,3)-1].name}}
     ได้ทั้งชื่อและ hp ออกมา
     {{randomDamage(1,300)}} สุ่ม 1 - 300-->
     <button v-on:click="$start" class="button-a" id="yellow">START</button>
     <button v-on:click="$attack" class="button-a" id="purple">ATTACK</button>
-    <button v-on:click="$special" class="button-a" id="purple">SPECIAL</button>
+    <button v-on:click="$special" class="button-a" id="purple">SPECIAL ATTACK</button>
   </div>
 </template>
 
@@ -28,18 +16,62 @@ export default {
     return {
       //เอาชื่อออกจาก object player[1].name
       //เอาชื่อออกจาก object player[random].name
-      aPlayer: { name: "", hp: 0, image: "" },
-      bMonster: { name: "", hp: 0, image: "" },
-      special: 0,
+
       player: [
-        { name: "Anakin", hp: 90, image: "a" },
-        { name: "Ironman", hp: 150, image: "b" },
-        { name: "Doreamon", hp: 200, image: "c" },
+        {
+          name: "Captain America",
+          hp: 235,
+          image: require("../assets/img/1CaptainAmerica.png"),
+        },
+
+        {
+          name: "Captain Marvel",
+          hp: 280,
+          image: require("../assets/img/1_2CaptainMarvel.png"),
+        },
+        {
+          name: "Hulk",
+          hp: 240,
+          image: require("../assets/img/1hulk.png"),
+        },
+        {
+          name: "Ironman",
+          hp: 250,
+          image: require("../assets/img/1ironman.png"),
+        },
+        {
+          name: "Thor",
+          hp: 255,
+          image: require("../assets/img/1Thor.png"),
+        },
       ],
+
       monster: [
-        { name: "Thanos", hp: 90, image: "x" },
-        { name: "Darth", hp: 10, image: "y" },
-        { name: "Comti", hp: 20, image: "z" },
+        {
+          name: "Batman",
+          hp: 230,
+          image: require("../assets/img/2batman.png"),
+        },
+        {
+          name: "Halyquen",
+          hp: 250,
+          image: require("../assets/img/2Halyquen.png"),
+        },
+        {
+          name: "Hellboy",
+          hp: 240,
+          image: require("../assets/img/2Hellboy.png"),
+        },
+        {
+          name: "Thanos",
+          hp: 260,
+          image: require("../assets/img/2thanos.png"),
+        },
+        {
+          name: "Wonder Woman",
+          hp: 240,
+          image: require("../assets/img/2_2wonder_woman.png"),
+        },
       ],
     };
   },
@@ -48,22 +80,54 @@ export default {
     randomNO: function (min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
+
     $start: function () {
-      //1.random Plaryer 1-3 -->ชื่อ ค่า hp รูป ฝากไว้ใน aPlary
-      this.aPlayer = this.player[this.randomNO(1, 3) - 1];
+      //สร้าง random
+      this.ranP = this.randomNO(1, 5) - 1;
+      this.ranM = this.randomNO(1, 5) - 1;
+      //1.random Plaryer 1-3 -->ชื่อ ค่า hp รูป
+      this.name_Player = this.player[this.ranP].name;
+      this.$emit("H_name", this.name_Player);
+      console.log("emit", this.name_Player);
+      this.hp_Player = this.player[this.ranP].hp;
+      this.$emit("H_hp", this.hp_Player);
+      console.log("hp", this.hp_Player);
+      this.image_Player = this.player[this.ranP].image;
+      this.$emit("H_image", this.image_Player);
+      console.log("im", this.image_Player);
+
       //2.random Monster 1-3
-       this.bMonster = this.monster[this.randomNO(1, 3) - 1];
+      this.name_monster = this.monster[this.ranM].name;
+      console.log("emit", this.name_monster);
+      this.$emit("M_name", this.name_monster);
+      this.hp_monster = this.monster[this.ranM].hp;
+      this.$emit("M_hp", this.hp_monster);
+      this.image_monster = this.monster[this.ranM].image;
+      this.$emit("M_image", this.image_monster);
     },
     $attack: function () {
       // p -> mon
-      this.attackP = this.randomNO(3,10);
+      this.atk = this.randomNO(3, 10);
+      this.$emit("P_atk", this.atk);
+      console.log("Attack :", this.atk);
 
+      this.atk = this.randomNO(3, 10);
+      this.$emit("M_atk", this.atk);
+      console.log("Atk back", this.atk);
       // p <- mon
       //p <=0 ===ทำไง  you lose
       //mon <==0 ทำไง you win
       //End
     },
-    $special: function () {},
+    $special: function () {
+      this.s_atk = this.randomNO(10, 20);
+      this.$emit("P_SATK", this.s_atk);
+      console.log("Special Attack :", this.s_atk);
+
+      this.s_atk = this.randomNO(10, 20);
+      this.$emit("M_SPATK", this.s_atk);
+      console.log("SPATK back", this.s_atk);
+    },
   },
 };
 </script>
@@ -79,8 +143,8 @@ export default {
   padding: 5px 5px 5px 5px;
   text-align: center;
   display: inline-block;
-  margin: 4px 2px;
-  font-size: 50px;
+  margin: 10px 3px;
+  font-size: 30px;
   cursor: pointer;
   border-radius: 10px;
   border: 0.5px solid white;
@@ -106,11 +170,7 @@ button:hover#yellow {
 }
 button:hover#purple {
   color: #9370db;
-}
-.a {
-  margin: auto;
-  width: 70%;
-  height: 500px;
-  border: 10px solid black;
+  border: 0.5px solid black;
+  background-color: black;
 }
 </style>
